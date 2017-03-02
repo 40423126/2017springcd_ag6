c440f4d66dd9d7dbceabbbcaf57be1bd55230e8a
40423126 <40423126@gm.nfu.edu.tw>
2017-03-02 10:41:40 +0800

    add blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = '2016Fall 電腦輔助設計實習 (虎尾科大MDE)'
+AUTHOR = 'ag6'
+SITENAME = '2017協同產品設計實習-設計二甲-第六組'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
